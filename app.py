d61f90e945ea73417f4f19e2beca1ee2c6e5a945
amitt1236 <62837431+amitt1236@users.noreply.github.com>
2020-07-10 17:27:58 +0300

    winner

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_session import Session
 from tempfile import mkdtemp
+import copy
+import math
 
 app = Flask(__name__)
 
@@ -15,8 +17,18 @@ def index():
     if "board" not in session:
         session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
         session["turn"] = "X"
+    if draw(session["board"]) == True:
+        return render_template("gameover.html", game=session["board"], draw=draw)
+    if winner(session["board"]) is not None:
+        return render_template("gameover.html", game=session["board"], winner=winner(session["board"]))
 
     return render_template("game.html", game=session["board"], turn=session["turn"])
+
+@app.route("/reset")
+def reset():
+    session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+    session["turn"] = "X"
+    return redirect(url_for("index"))
 
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
@@ -27,8 +39,21 @@ def play(row, col):
         session["turn"] = "X"   
     return redirect(url_for("index"))
 
-@app.route("/reset")
-def reset():
-    session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
-    session["turn"] = "X"
-    return redirect(url_for("index"))
+def winner(board):
+    # check colums
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != None:
+            return board[i][0]
+    # check rows
+    for i in range(3):
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != None:
+            return board[0][i]
+    # check diagonal
+    if board[0][0] == board[1][1] == board[2][2] or board[2][0] == board[1][1] == board[0][2] and board[1][1] != None:
+        return board[1][1]
+
+    return None
+
+def draw(board):
+    if sum(j.count(None) for j in board) == 0 and winner(board) is None:
+        return True    
