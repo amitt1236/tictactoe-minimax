80d3c7c30756f955297fa70dd1fcf4859a908118
amitt1236 <62837431+amitt1236@users.noreply.github.com>
2020-07-11 12:41:43 +0300

    working2

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask_session import Session
 from tempfile import mkdtemp
 
 app = Flask(__name__)
+app.secret_key = "dev"
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app.secret_key = "dev"
+
 
 @app.route("/")
 def index():
